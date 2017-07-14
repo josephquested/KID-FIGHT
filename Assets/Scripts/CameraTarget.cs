e746be1e3a06e7fe0ffdc947ae1d657823e2622d
Joseph Quested <josephquested@gmail.com>
2017-07-14 21:08:12 +1200

    add pistol casing ejection

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour {
+
+	// SYSTEM //
+
+	PlayerController player;
+
+	public Vector2 rightPosition;
+	public Vector2 leftPosition;
+
+	void Start ()
+	{
+		player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+	}
+
+	void Update ()
+	{
+		if (player.facingRight) transform.localPosition = rightPosition;
+		else { transform.localPosition = leftPosition; }
+	}
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -43,6 +43,12 @@ public class Weapon : MonoBehaviour {
 
 	public void ReceiveFire ()
 	{
+		InstantiateProjectile();
+		EjectCasing();
+	}
+
+	void InstantiateProjectile ()
+	{
 		GameObject projectile = Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
 		projectile.GetComponent<SpriteRenderer>().flipX = !player.facingRight;
 		projectile.GetComponent<Rigidbody2D>().AddForce(ProjectileForce() * projectileSpeed);
@@ -52,5 +58,31 @@ public class Weapon : MonoBehaviour {
 	{
 		if (player.facingRight) return Vector2.right;
 		else return Vector2.left;
+	}
+
+	// EFFECTS //
+
+	public GameObject casingPrefab;
+	public Transform casingSpawn;
+
+	public Vector2 spawnRightPosition;
+	public Vector2 spawnLeftPosition;
+
+	void EjectCasing ()
+	{
+		GameObject casing = Instantiate(casingPrefab, casingSpawn.position, casingSpawn.rotation);
+
+		float forceX = Random.Range(-5, 0);
+		casingSpawn.localPosition = spawnRightPosition;
+
+		if (!player.facingRight)
+		{
+			casingSpawn.localPosition = spawnLeftPosition;
+			forceX = Random.Range(5, 0);
+		}
+
+		Vector2 force = new Vector2(forceX, Random.Range(2, 5));
+		casing.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+		casing.GetComponent<Rigidbody2D>().angularVelocity = Random.Range(100, 900);
 	}
 }
